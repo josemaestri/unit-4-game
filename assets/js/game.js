e4ac87afe60364095db2c19141fd43da6a6a79aa
josemaestri <maestri.jose@gmail.com>
2018-09-18 14:21:59 -0400

    js complete, css started

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,118 +1,238 @@
-// GLOBAL VARIABLES
-// ##########################################
-var fighters = [
-  {
-    id: 'clu',
-    name: 'Clu',
-    health: 125,
-    attack: 5,
-    xp: 1
-  },
-  {
-    id: 'tron',
-    name: 'Tron/Rinzler',
-    health: 150,
-    attack: 10,
-    xp: 1
-  },
-  {
-    id: 'quorra',
-    name: 'Quorra',
-    health: 175,
-    attack: 15,
-    xp: 1
-  },
-  {
-    id: 'zuse',
-    name: 'Zuse/Castor',
-    health: 200,
-    attack: 20,
-    xp: 1
-  },
-
-];
-
-var champion;
-var defender;
-var enemies = [];
-var rounds = enemies.length;
-
-// HTML VARIABLES
-// ##########################################
 
 
-// FUNCTIONS
-// ##########################################
 
-// print characters
+// GET READY
+$(document).ready(function() {
 
-// select champion (user)
+  // GLOBAL VARIABLES
+  // ##########################################
+  var fighters;
 
-// print champion onto HTML
+  var champion;
+  var defender;
+  var enemies = [];
+  var rounds;
 
-// print enemies onto HTML
 
-// select defender (user)
-function selectDef(){
-  console.log('new player');
-}
+  // HTML VARIABLES
+  // ##########################################
+  var areaFighters = $('.area-fighters');
+  var areaPlay = $('.area-play');
+  var rowFighters = $('.row-fighters');
+  var rowPlay = $('.row-play');
+  var colPlay = $('.col-play');
+  var colEnemies = $('.col-enemies');
+  var rowAttack = $('.row-attack');
+  var champEl = $('.champion');
+  var defEl = $('.defender');
 
-// print defender
 
-// attack (user)
-function attack(champ, def) {
+  // FUNCTIONS
+  // ##########################################
 
-  var damage = champ.xp * champ.attack;
-
-  def.health = def.health - damage;
-  champ.xp++;
-  
-  checkIfDead(def);
-
-  if(!def.isDead){
-    defend(def, champ);
-  } else{
-    rounds--;
-    if(rounds > 0){
-      selectDef();
-    } else{
-      handleWin();
+  // print characters
+  function printFighters(){
+    for (var i = 0; i < fighters.length; i++) {
+      var el = $('<div class="col"><div class="fighter" data-fighter="'+fighters[i].id+'" data-fighter-pos="'+i+'">'+fighters[i].name+'</div></div>');
+      el.appendTo('.row-fighters');
     }
   }
 
-  checkIfDead(champ);
-
-  if(champ.isDead){
-    handleLoss(); 
+  // select champion (user)
+  function selectChamp(fighterPos){
+    champion = fighters[fighterPos];
+    for (var i = 0; i < fighters.length; i++) {
+      if (i !== fighterPos){
+        enemies.push(fighters[i]);
+      }
+    }
+    rounds = enemies.length;
+    areaFighters.fadeOut(1000,printChampion);
   }
 
-  console.log('Champ Health: ' + champ.health);
-  console.log('Def Health: ' + def.health);
-}
-
-// defend 
-function defend(def, champ) {
-  var damage = def.xp * def.attack;
-  champ.health = champ.health - damage;
-}
-
-// check if fighter has died
-function checkIfDead(fighter){
-  if (fighter.health > 0){
-    return true;
-  } else {
-    fighter.isDead = true;
+  // print champion onto HTML
+  function printChampion(){
+    champEl.html(champion.name);
+    colPlay.removeClass('hide').addClass('show');
+    areaPlay.fadeIn(1000, function(){
+      printEnemies();  
+    });
   }
-}
 
-// handle win
-function handleWin(){
-  console.log('You Win!');
-}
+  // print enemies onto HTML
+  function printEnemies(){
+    for (var i = 0; i < enemies.length; i++) {
+      var en = $('<div class="fighter enemy" data-fighter-pos="'+fighters.indexOf(enemies[i])+'">'+enemies[i].name+'</div>');
+      en.appendTo(colEnemies);
+      colEnemies.removeClass('hide').addClass('show');
+    }
+  }
 
-// handle loss
-function handleLoss(){
-  console.log('Womp. You Lose.');
-}
+  // select defender (user)
+  function selectDef(enPos){
+    defender = fighters[enPos];
+    var defEl = $('.enemy[data-fighter-pos="'+enPos+'"');
+    defEl.hide();
+    printDef();
+  }
 
-// reset game
+  // print defender
+  function printDef(){
+    defEl.html(defender.name);
+    rowAttack.removeClass('hide').addClass('show');
+  }
+
+  // attack (user)
+  function attack(champ, def) {
+
+    var damage = champ.xp * champ.attack;
+
+    def.health = def.health - damage;
+    champ.xp++;
+    
+    checkIfDead(def);
+
+    if(!def.isDead){
+      defend(def, champ);
+    } else{
+      rounds--;
+      if(rounds > 0){
+        alert('Select New Defender');
+      } else{
+        handleWin();
+      }
+    }
+
+    checkIfDead(champ);
+
+    if(champ.isDead){
+      handleLoss(); 
+    }
+
+    console.log('Champ Health: ' + champ.health);
+    console.log('Def Health: ' + def.health);
+  }
+
+  // defend 
+  function defend(def, champ) {
+    var damage = def.xp * def.attack;
+    champ.health = champ.health - damage;
+  }
+
+  // check if fighter has died
+  function checkIfDead(fighter){
+    if (fighter.health > 0){
+      return true;
+    } else {
+      fighter.isDead = true;
+    }
+  }
+
+  // handle win
+  function handleWin(){
+    var playAgain = confirm('Woo! You Win! Play Again?');
+
+    if(playAgain){
+      reset();
+    }
+  }
+
+  // handle loss
+  function handleLoss(){
+    var playAgain = confirm('Womp. You Lose. Play Again?');
+
+    if(playAgain){
+      reset();
+    }
+  }
+
+  // reset/init game
+  function reset(){
+    areaPlay.fadeOut(1000,function(){
+      colPlay.removeClass('show').addClass('hide');
+      colEnemies.removeClass('show').addClass('hide');
+      rowAttack.removeClass('show').addClass('hide');
+
+      rowFighters.empty();
+      defEl.empty();
+      champEl.empty();
+      $('.enemy').remove();
+
+      init();
+    });
+  }
+
+  function init(){
+
+    areaFighters.fadeIn(1000);
+
+    fighters = [
+      {
+        id: 'clu',
+        name: 'Clu',
+        health: 125,
+        attack: 5,
+        xp: 1
+      },
+      {
+        id: 'tron',
+        name: 'Tron/Rinzler',
+        health: 150,
+        attack: 10,
+        xp: 1
+      },
+      {
+        id: 'quorra',
+        name: 'Quorra',
+        health: 175,
+        attack: 15,
+        xp: 1
+      },
+      {
+        id: 'zuse',
+        name: 'Zuse/Castor',
+        health: 200,
+        attack: 20,
+        xp: 1
+      }
+    ];
+
+    champion = '';
+    defender = '';
+    enemies = [];
+    rounds = 0;
+
+    printFighters();
+  }
+
+
+
+
+
+
+  // RUN THE TRAPPPPPPPPPPPP
+  // ##########################################
+
+  // start game
+  init();
+  var fighter = rowFighters.find('.fighter');
+  
+
+
+  // user selects a fighter
+  $(document).on('click', '.row-fighters .fighter', function(){
+    var pos = $(this).data('fighter-pos');
+    selectChamp(pos);
+  });
+
+  // user selects a defender
+  $(document).on('click','.enemy', function(){
+    var pos = $(this).data('fighter-pos');
+    selectDef(pos);
+  });
+
+  // user attacks
+  $(document).on('click', '.attack', function(){
+    attack(champion,defender);
+  });
+});
